13be38ba22dd39ebee53a09073ed746fa86e9ba4
Rahul Nishad <nishadrahul468@gmail.com>
2024-07-11 17:47:32 +0530

    database env set

---
+++ config/config.js
@@ -0,0 +1,23 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: './config.env' });
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    port: process.env.DB_PORT
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    port: process.env.DB_PORT
+  },
+};
+
+
